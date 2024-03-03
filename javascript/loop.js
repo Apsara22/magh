2c4e5b7fa56e52061738f77c304785e6d512b209
Apsara <paneruapsara831ap@gmail.com>
2024-03-03 16:48:48 +0545

    update today

--- javascript/loop.js
+++ javascript/loop.js
@@ -32,6 +32,65 @@ let topics2 =[
  }
  //console.log(`${topics2[0].name} is ${topics2[0].status}`)
 
- //function  findMultiplication() multiply of five,ten
+ function  findMultiplication(num, multipler =1) {//multiply of five,ten
+if(multipler>10)
+{
+   return;
+}
+console.log(`${num} * ${multipler} =${num * multipler}`);
+findMultiplication(num, multipler+1);
+}
+findMultiplication(5);
+console.log("print table 10");
+findMultiplication(10);
 
- 
+
+function multipleTable(input){
+   for (let sp=5; sp<11; sp++){
+      let result =input+sp;
+
+      console.log(`${input} * ${sp} =${result}`);
+   }
+
+
+}
+multipleTable(5)
+
+ /*findMultiply({
+   number:10,
+   startForm: 5,
+   stopAt: 10
+ )};*/
+
+ function findMultiply(input,startForm,endAt){
+   for(let sp=startForm; sp<=endAt; sp++){
+
+   let result =input+sp;
+
+      console.log(`${input} * ${sp} =${result}`);
+   }
+ }
+ findMultiply(5)
+
+ findMultiply({
+   number:10,
+   startForm: 5,
+   stopAt: 10
+ });
+ findMultiply(5)
+
+ findMultiply({
+   number:12,
+   startForm: 1,
+   stopAt: 10
+ });
+
+ //object destructuring
+//  let input=filterObj.input;
+//  let startForm = filterObj.startForm;
+
+let numbers = [1,2,3,4,5]
+
+for(i=1;i<numbers.length;i++){
+   console.log("the print number is",numbers[i]*2);
+}
