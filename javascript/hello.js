29671f35c082665e4b0c7f3fb769fb465214da5a
Apsara <paneruapsara831ap@gmail.com>
2024-03-17 17:00:55 +0545

    push code

--- javascript/hello.js
+++ javascript/hello.js
@@ -1,1 +1,40 @@
-console.log("hello")
+let numbers =[6,5,4,3,2,1,0];
+let evenNumbers=[]
+
+for(index=0; index<numbers.length; index++){
+    let elements = numbers[index];
+    if(elements % 2 ===0){
+        console.log(elements);
+    }
+    else{
+        console.log(elements);
+    }
+}
+
+let todos = [
+    {
+      title: "html",
+      status: true,
+    },
+    {
+      title: "css",
+      status: true,
+    },
+    {
+      title: "js",
+      status: true,
+    },
+    {
+      title: "react",
+      status: false,
+    },
+  ];
+// console.log(todos[1].title)
+for(i=0; i<todos.length; i++){
+if(todos[i].status==true){
+    console.log(todos[i].title)
+}
+else{
+    console.log("false")
+}
+}
