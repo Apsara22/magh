9413d338d70965cdead6e69ab5d822d240133d00
Apsara <paneruapsara831ap@gmail.com>
2024-03-20 16:36:43 +0545

    tenerary operation

--- javascript/asynchronous.js
+++ javascript/asynchronous.js
@@ -53,3 +53,123 @@ let todos = [
    })
 
 
+let cardItems =[
+  {
+    product_id : "watch",
+    quantity: 1,
+    rate: 20,
+  },
+  {
+    product_id : "mobile",
+    quantity: 1,
+    rate: 10,
+  }
+
+];
+//let printSumtotal = [];
+sumtotal =0;
+
+cardItems.forEach((element) => {
+
+  for(i=0; i<=cardItems.length; i++)
+  sumtotal =sumtotal+(element.rate * element.quantity);
+
+})
+console.log(`the total sum is ${sumtotal}`);
+
+
+let users = [
+  {
+    name: "ram",
+    cartItems: [
+      {
+        product: "watch",
+        quantity: 1,
+        rate: 1000,
+      },
+      {
+        product: "mouse",
+        quantity: 2,
+        rate: 2000,
+      },
+    ],
+  },
+  {
+    name: "hari",
+    cartItems: [
+      {
+        product: "keyboard",
+        quantity: 1,
+        rate: 2000,
+      },
+      {
+        product: "mouse",
+        quantity: 2,
+        rate: 2000,
+      },
+    ],
+  },
+];
+
+/* ram has order of rs:  /
+/ hari has order of rs:  */
+
+ users.forEach((user) =>
+ {
+  let total=0;
+   user.cartItems.forEach((item) =>{
+    total = total + item.rate * item.quantity
+   })
+   console.log(`${user.name} has ordered rs: ${total}`);
+ })
+
+ 
+ let users1 = [
+  {
+    name: "ram",
+    cartItems: [
+      {
+        product: "watch",
+        quantity: 1,
+        rate: 1000,
+      },
+      {
+        product: "mouse",
+        quantity: 2,
+        rate: 2000,
+      },
+    ],
+  },
+  {
+    name: "hari",
+    cartItems: [
+      {
+        product: "keyboard",
+        quantity: -1,
+        rate: 2000,
+      },
+      {
+        product: "mouse",
+        quantity: -2,
+        rate: 2000,
+      },
+    ],
+  },
+];
+
+/* ram has order of rs:  /
+/ hari has order of rs:  */
+
+ users1.forEach((user) =>
+ {
+  let total=0;
+   user.cartItems.forEach((item) =>{
+  if(item.quantity>=0){
+    total = total + item.rate * item.quantity
+   }})
+   console.log(`${user.name} has ordered rs: ${total}`);
+ })
+
+
+
+ 
