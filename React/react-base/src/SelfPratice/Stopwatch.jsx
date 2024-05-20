049b5fc4056d0d04c8f7d381baea0952b62ea7a2
Apsara <paneruapsara831ap@gmail.com>
2024-05-20 15:03:36 +0545

    push db code

--- React/react-base/src/SelfPratice/Stopwatch.jsx
+++ React/react-base/src/SelfPratice/Stopwatch.jsx
@@ -1,13 +1,46 @@
-import { useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 export default function Stopwatch(){
     const[time, setTime]= useState(0)
-    const [isRunning, setRunning] = useState(false)
+    const [isRunning, setIsRunning] = useState(false)
+    const intervalRef = useRef(null)
+
+    useEffect(()=> {
+        if(isRunning){
+            intervalRef.current = setInterval(()=>{
+                setTimer((prevTimer)=> prevTimer=10);
+            },10);
+        }
+        else if (!isRunning){
+            clearInterval(intervalRef.current);
+        }
+        return()=> clearInterval(intervalRef.current)
+    },[isRunning]);
+
+    const startTimer = () =>{
+        setIsRunning(true);
+    }
+    const stopTimer = () => {
+        setIsRunning(false);
+      };
+    
+      const resetTimer = () => {
+        setTimer(0);
+      };
+    
+      const formatTime = (timer) => {
+        const getSeconds = `0${(timer / 1000) % 60}`.slice(-2);
+        const minutes = `${Math.floor(timer / 60000)}`.padStart(2, '0');
+        const milliseconds = `0${(timer % 1000)}`.slice(-3);
+    
+        return `${minutes} : ${getSeconds} : ${milliseconds}`;
+      };
+
     return(
         <div>
-            <button>Start</button>
-            <button>Stop</button>
-            <button>Reset</button>
+            <button onClick={startTimer}>Start</button>
+            <button onClick={stopTimer}>Stop</button>
+            <button onClick={resetTimer}>Reset</button>
         </div>
 
     )
--- TODOS-API/todos-api.js
+++ TODOS-API/todos-api.js
@@ -14,14 +14,50 @@ const Todo = mongoose.model('Todo',TOdosSchema);
 
 mongoose.connect('mongodb://127.0.0.1:27017/todos')
   .then(() => console.log('Connected!'));
+  app.use(express.json())
 
-app.get("/api/todoes", asyn(req, res) => {
-  let Todo = await Todo.find()
-  console.log(todo);
+app.get("/api/todos", async(req, res) =>{
+  let todos =await Todo.find()
+  console.log(todos);
   // res.send(["html","css"])
   res.send(todos)
 })
+app.post("/api/todos", async (req, res) => {
+  // db.todos.insertOne({title:})
+  try {
+    let newTodo = await Todo.create({
+      title: req.body.title,
+      status: false,
+    })
+    res.send(newTodo)
+    res.send("data added")
+  } catch (err) {
+    console.log(err)
+    res.status(500).send("server error")
+  }
+})
+
+app.put("/api/todos/:id", async (req, res) => {
   
+  try {
+    let todo = await Todo.findByIdAndUpdate(req.params.id, {
+      title: req.body.title,
+      status: req.body.status,
+    })
+
+    res.send("data updated.")
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("server error")
+  }
+})
+app.delete("/api/todos/:id", async (req, res) => {
+  
+
+  let todo = await Todo.findByIdAndDelete(req.params.id)
+  res.send("data deleted.")
+})
+
 
 app.listen(8000,()=>{
     console.log("... Server Started")
